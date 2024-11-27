9be879713ef327df86d4b920622fda6af3ee2f9e
Guille P. Corbella <guillepcorbella@hotmail.com>
2024-11-27 18:51:00 +0100

    Done until 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './components/Filter'
 import './components/Persons'
 import './components/PersonForm'
@@ -8,15 +9,21 @@ import Persons from './components/Persons'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const handleNameChange = (e) => setNewName(e.target.value)
   const handleNumberChange = (e) => setNewNumber(e.target.value)
@@ -30,15 +37,15 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons[persons.length-1].id + 1
+      id: persons[persons.length - 1].id + 1
     }
 
     if (persons.find(e => e.name === newName)) {
       alert(`${newName} is already added to phonebook`)
-    } 
+    }
     else if (persons.find(e => e.number === newNumber)) {
       alert(`${newNumber} is already added to phonebook`)
-    } 
+    }
     else {
       setPersons(persons.concat(personObject))
       setNewName('')
@@ -52,7 +59,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <div>debug: {newFilter}</div>
       <Filter
-      handleFilterChange={(e) => handleFilterChange(e)}
+        handleFilterChange={(e) => handleFilterChange(e)}
       ></Filter>
       <h2>Add a New</h2>
       <div>debug: {newName}</div>
